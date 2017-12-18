aa6097b323502682f34f258c33c85764c955582e
melissayauri <yaurimelissa@gmail.com>
2017-12-17 22:33:36 -0500

    incorporando modal iniciar sesion

--- js/jquery1.js
+++ js/jquery1.js
@@ -1,2 +1,6 @@
 /*funcionalidad para que menu hamburguesa muestre la lista despegable*/
  $(".button-collapse").sideNav();
+/*modal iniciar sesion*/
+$(document).ready(function(){
+    $('#iniciar-sesion').modal();
+  });
